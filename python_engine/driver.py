e6267a36fb224df46a3635c08d302d69308fe29a
wrongu <richard.d.lange.13@dartmouth.edu>
2014-05-15 14:09:34 -0400

    driver autoruns all matches and saves results in stats.txt

--- python_engine/driver.py
+++ python_engine/driver.py
@@ -1,12 +1,7 @@
 #!/usr/bin/python
 import maze
-import time
-from sys import exit
 import subprocess
 import itertools
-
-pipe_in = open('temp_in', 'rw')
-pip_out = open('temp_out', 'rw')
 
 # DEFINE THE ARCHITECT AND SOLVER HERE
 # Architect is a program which takes input via stdin (as spec'd in readme) and outputs a maze string
@@ -24,32 +19,12 @@ solvers = {
 
 SIZE = 20
 SEED = 42
+VERBOSE = True
 STEP_LIMIT = SIZE*SIZE*SIZE
-
-# Comment these lines if you don't want the same output every time
-import random
-random.seed(6)
 
 ########################################################
 #### everything below this line is not customizable ####
 ########################################################
-
-print "constructing architect subprocess"
-architect_input  = "%d %d\n" % (SIZE, SEED)
-kargs = architects['greek']
-kargs.update({
-		'stdin' : subprocess.PIPE,
-		'stdout' : subprocess.PIPE,
-		'stderr':subprocess.STDOUT
-	})
-architect_process = subprocess.Popen(**kargs)
-architect_output = architect_process.communicate(input=architect_input)[0]
-m = maze.Maze.load_from_architect_string(architect_output)
-print m.ascii_str()
-
-if not m.is_valid():
-	print "Maze is invalid. Exiting."
-	exit(1)
 
 def maze_score(maze, steps_taken):
 	L1_dist = abs(maze._start[0] - maze._end[0]) + abs(maze._start[1] - maze._end[1])
@@ -57,28 +32,54 @@ def maze_score(maze, steps_taken):
 	n_walls = maze.num_walls()
 	return float(steps_taken) / float(max(L1_dist, n_walls))
 
-for steps in xrange(STEP_LIMIT):
-	if m.solved():
-		print "Solver finished in %d steps" % steps
-		break
-	
-	solver_input = m.to_solver_string()
-	kargs = solvers['greek']
-	kargs.update({
-		'stdin' : subprocess.PIPE,
-		'stdout' : subprocess.PIPE,
-		'stderr':subprocess.STDOUT
-	})
-	solver_process = subprocess.Popen(**kargs)
-	solver_output = solver_process.communicate(input=solver_input)[0]
-	print "SOLVER OUTPUT='%s'" % solver_output
-	decision = tuple(map(int,solver_output.split()))
-	if decision in m.options():
-		m.move_to(decision)
-	else:
-		print "Solver tried to cheat!"
-	# COMMENT THIS TO TURN OFF ANIMATION
-	print m.ascii_str(fog=True); time.sleep(0.1)
-else: # 'nobreak' clause of for loop
-	print "Solver could not finish the maze in %d steps" % steps
-print "Score: %f" % maze_score(m, steps)
+# get all pairs of architect+solver
+all_matches = itertools.product(architects.keys(), solvers.keys())
+architect_input  = "%d %d\n" % (SIZE, SEED)
+
+with open('stats.txt', 'w') as f:
+	for arch_name, solver_name in all_matches:
+		try:
+			if VERBOSE: print "beginning match", arch_name, "vs", solver_name
+			kargs = architects[arch_name]
+			kargs.update({
+					'stdin' : subprocess.PIPE,
+					'stdout' : subprocess.PIPE,
+					'stderr':subprocess.STDOUT
+				})
+			architect_process = subprocess.Popen(**kargs)
+			architect_output = architect_process.communicate(input=architect_input)[0]
+			m = maze.Maze.load_from_architect_string(architect_output)
+			if VERBOSE: print m.ascii_str()
+
+			if not m.is_valid():
+				raise AssertionError("Invalid Maze")
+
+			for steps in xrange(STEP_LIMIT):
+				if m.solved():
+					if VERBOSE: print "Solver finished in %d steps" % steps
+					break
+				elif steps % SIZE == 0 and VERBOSE:
+					print steps
+				
+				solver_input = m.to_solver_string()
+				kargs = solvers['greek']
+				kargs.update({
+					'stdin' : subprocess.PIPE,
+					'stdout' : subprocess.PIPE,
+					'stderr':subprocess.STDOUT
+				})
+				solver_process = subprocess.Popen(**kargs)
+				solver_output = solver_process.communicate(input=solver_input)[0]
+				decision = tuple(map(int,solver_output.split()))
+
+				if decision in m.options():
+					m.move_to(decision)
+				elif VERBOSE: print "Solver tried to cheat!" # not an exception.. already limited by STEP_LIMIT
+			else: # 'nobreak' clause of for loop
+				if VERBOSE: print "Solver could not finish the maze in %d steps" % steps
+			score = maze_score(m, steps)
+			scoreline = "%s,%s,%d,%d,%f,%f" % (arch_name, solver_name, SIZE, SEED, score, 1/score)
+			if VERBOSE: print scoreline
+			f.write(scoreline+"\n")
+		except Exception as e:
+			print "Problem with match", arch_name, "vs", solver_name, "::", e
